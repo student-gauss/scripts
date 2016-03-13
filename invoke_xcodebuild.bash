34506446a84173acbae6782f7f0dfa266d339396
Student Gauss <student.gauss@gmail.com>
2016-03-13 09:05:55 -0700

    Support workspace build.

--- invoke_xcodebuild.bash
+++ invoke_xcodebuild.bash
@@ -6,9 +6,23 @@ project_directory=$PWD
 until [ "$project_directory" = "/" ] ; do
     FILTER=$(find $project_directory -maxdepth 1 \( -name "*.xcodeproj" \) )
     if [ ${FILTER} ] ; then
-        xcodebuild -project ${FILTER} -alltargets
+        project_directory=${FILTER}
+
+        FILTER=$(find $project_directory -maxdepth 1 \( -name "*.xcworkspace" \) )
+
+        if [ ${FILTER} ] ; then
+            # The project uses a built-in workspace
+            builtin_workspace=${FILTER}
+
+            # The scheme name is assumed to be same as the project name
+            scheme_name=$(basename ${project_directory} .xcodeproj)
+
+            xcodebuild -workspace ${builtin_workspace} -scheme ${scheme_name}
+        else
+            xcodebuild -project ${project_directory} -alltargets
+        fi
+
         break
     fi
     project_directory=$(dirname $project_directory)
 done
-
